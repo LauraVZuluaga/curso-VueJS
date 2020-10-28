fa9f8251361a1678276f93ec849c6eb276481a97
laura.1701614930@ucaldas.edu.co <laura.1701614930@ucaldas.edu.co>
2020-10-28 00:19:35 -0500

    Avances introductorios

--- Conceptos/main.js
+++ Conceptos/main.js
@@ -2,8 +2,29 @@ const app = Vue.createApp({
     data(){
         return{
             titulo: 'Mi banco con VueJs',
-            cantidad:1000, 
-            enlace: 'https://youtube.com'
+            cantidad: 1000, 
+            enlace: 'https://youtube.com', 
+            estado: false, 
+            servicios: ['transferencias', 'pago', 'giros'], 
+            desactivar: false
+        }
+    }, 
+    methods: {
+        agregarSaldo(){
+            this.cantidad = this.cantidad + 100
+        }, 
+        disminuirSaldo(valor){
+            if (this.cantidad === 0){
+                alert('Saldo en cero')
+                this.desactivar = true
+                return
+            }
+            this.cantidad = this.cantidad - valor
+        }
+    }, 
+    computed:{
+        colorCantidad(){
+            return this.cantidad > 500 ? 'text-success' : 'text-danger'
         }
     }
 })
